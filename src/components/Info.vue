5145876544de8a003003c71c2814eac74ba8c906
jasiel <jasiel_serra@hotmail.com>
2022-12-18 10:04:47 -0300

    Diretivas v-if and v-show

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <p>Estou trabalhando no momento.</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidades</p>
     <p>Utilizo as seguintes tecnologias:</p>
     <ul>
       <li>Python</li>
       <li>Django Framework</li>
       <li>Vue-JS</li>
     </ul>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{email}}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Info"
+  name: "Info",
+  data(){
+    return {
+      esta_trabalhando: true,
+      mostrar_email:false,
+      email: 'jasiel_serra@hotmail.com'
+    }
+  }
 }
 </script>
