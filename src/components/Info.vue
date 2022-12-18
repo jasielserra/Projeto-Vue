b3d5fc741dab92fe3cc8d81631979f04599fc2d5
jasiel <jasiel_serra@hotmail.com>
2022-12-18 17:32:51 -0300

    Atributos Dinâmicos

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
-    <p v-else>Estou em busca de novas oportunidades</p>
+    <p v-else>Estou em busca de novas oportunidades!</p>
     <p>Utilizo as seguintes tecnologias:</p>
     <ul>
       <li>Python</li>
@@ -9,17 +9,22 @@
       <li>Vue-JS</li>
     </ul>
     <p v-show="mostrar_email">Mande uma mensagem para: {{email}}</p>
+    <p>Para acessar meu portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a> </p>
+    <Picture/>
   </div>
 </template>
 
 <script>
+import Picture from "@/components/Picture";
 export default {
   name: "Info",
+  components: {Picture},
   data(){
     return {
       esta_trabalhando: true,
       mostrar_email:false,
-      email: 'jasiel_serra@hotmail.com'
+      email: 'jasiel_serra@hotmail.com',
+      meu_link:"https://github.com/jasielserra",
     }
   }
 }
