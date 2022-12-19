8e31cf4751799a2e5d7f9068699fff97301a498f
jasiel <jasiel_serra@hotmail.com>
2022-12-19 15:19:19 -0300

    Reutilização de componentes.

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,10 +1,16 @@
 <template>
   <img :src="avatar" :alt="descricao">
+  <Reutilizacao/>
 </template>
 
 <script>
+import Reutilizacao from "@/components/Reutilizacao";
+
 export default {
   name: "Picture",
+  components: {
+    Reutilizacao
+  },
   data(){
     return {
       avatar: "/img/jasi.jpeg",
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -3,16 +3,22 @@
     <h1> Olá Vue! </h1>
     <p>Meu nome é {{nome}} e trabalho  como {{profissao}}</p>
     <MultiplosEventos/>
+    <Reutilizacao/>
+    <Reutilizacao/>
+    <Reutilizacao/>
+    <Reutilizacao/>
   </div>
 </template>
 
 <script>
   import MultiplosEventos from './MultiplosEventos.vue';
+  import Reutilizacao from "@/components/Reutilizacao";
 
   export default {
      name: 'PrimeiroComponente',
      components: {
-       MultiplosEventos
+       MultiplosEventos,
+       Reutilizacao
      },
      data() {
        return {
